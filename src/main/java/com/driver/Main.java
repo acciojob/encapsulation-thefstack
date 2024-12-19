3295cd03c25457c5cca6f215a215964ca84993ef
thefstack <rajsharmahwh19@gmail.com>
2024-12-19 20:00:21 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj=new RWOnly();
+    obj.setName("Raj");
+    System.out.println(obj.getName());
+  }
 }
